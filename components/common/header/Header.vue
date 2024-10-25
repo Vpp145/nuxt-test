19e6fbe6d4736144437249fe6e8518c35e676960
Vyom Patel <vyomp59@gmail.com>
2024-10-25 14:59:53 +0530

    routing and seo added

---
+++ components/common/header/Header.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/post/1">Post 1</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- pages/app.vue
+++ pages/app.vue
@@ -1,4 +1,14 @@
-<script></script>
+<script setup lang="ts">
+// useHead({
+//   title: "Nuxt 3 starter",
+//   meta: [
+//     { name: "description", content: "This is a description of your page" },
+//     { name: "keywords", content: "Nuxt, Vue, Example" },
+//     { property: "og:title", content: "Your Open Graph Title" },
+//     { property: "og:description", content: "Your Open Graph description" },
+//   ],
+// });
+</script>
 
 <template>
   <div>
